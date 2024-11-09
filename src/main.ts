99cfbbd8778cd3b8b9fec77bb37d8b6f14ab9344
Bayan-AlWardi <123604689+Bayan-AlWardi@users.noreply.github.com>
2024-11-09 00:34:11 +0000

    update doc

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { TaskDetailsComponent } from './app/task-details/task-details.component';
 import { HomeComponent } from './app/home/home.component';
 
+
 const routes = [
   { path: '', component: HomeComponent }, // Route for home view (task list and add task)
   { path: 'task/:id', component: TaskDetailsComponent } // Route for task details with 'id' parameter
